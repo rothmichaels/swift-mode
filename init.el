4b109a21e8d05e16f204f50d7d99103922760468
Roth Michaels <roth@rothmichaels.us>
2015-11-24 20:43:23 -0500

    Added swift-mode submodule

--- init.el
+++ init.el
@@ -3,11 +3,8 @@
 ;; Use this file to initiate the pack configuration.
 ;; See README for more information.
 
-(live-add-pack-lib "scala-mode2")
-(require 'scala-mode2)
-
-(live-add-pack-lib "sbt-mode")
-(require 'sbt-mode)
+(live-add-pack-lib "swift-mode")
+(require 'swift-mode)
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
